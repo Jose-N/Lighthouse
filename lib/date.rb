cdb4052b074818746dbcd432b9e0cc47282dfeb9
Jose Naylor <jose.c.naylor@gmail.com>
2017-05-31 06:51:31 -0400

    date can now receive input and return todays date or a single specificed date

--- lib/date.rb
+++ lib/date.rb
@@ -4,8 +4,10 @@ def grab_dates(*date)
     if date.size == 1
         if date[0] == 'today'
             puts Date.today
+        else
+            puts date[0]
         end
     end
 end
 
-grab_dates('today')
+grab_dates('2017-05-30')
